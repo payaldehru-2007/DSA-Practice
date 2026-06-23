d059406c3bf68de4e13d5ece77e4e143aa21b343
payaldehru-2007 <payaldehru@gmail.com>
2026-06-23 18:37:27 +0530

    Add problem 3 (day of week) and problem 4 (array basics)

--- 1_basics/things_to_know.py
+++ 1_basics/things_to_know.py
@@ -23,3 +23,42 @@ elif marks>=35 and marks<50:
 else:
     print("fail")
  '''
+
+##problem 3##
+##Given the integer day denoting the day number, print on the screen which day of the week it is. Week starts from Monday and for values greater than 7 or less than 1, print Invalid.
+'''
+day = int(input("enter the day number :"))
+match day:
+    case 1:
+        print("monday")
+    case 2:
+        print("tuesday")
+    case 3:
+        print("wednesday")
+    case 4:
+        print("thursday")
+    case 5:
+        print("friday")
+    case 6:
+        print("saturday")
+    case 7:
+        print("sunday")
+    case_:
+        print("invalid")    
+'''
+
+## problem 4##
+##Take 5 integers as input from the user and store them in an array (list).Then print:
+##The complete array. The first element. The last element. The element at index 2.
+'''
+arr1 = int(input("Enter the number: "))
+arr2 = int(input("Enter the number: "))
+arr3 = int(input("Enter the number: "))
+arr4 = int(input("Enter the number: "))
+arr5 = int(input("Enter the number: "))
+arr = [arr1, arr2, arr3, arr4, arr5]
+print(arr)
+print(arr[0])
+print(arr[4])
+print(arr[2])
+'''
