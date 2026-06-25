70c6e24577b71e527c0cdeb9b40957a528e91abc
payaldehru-2007 <payaldehru@gmail.com>
2026-06-25 16:48:49 +0530

    Day 2: Patterns P1-P3 (rectangle, star triangle, number triangle)

---
+++ 1_basics/patterns.py
@@ -0,0 +1,46 @@
+## problem 1##
+##Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N =5
+'''
+*****
+*****
+*****
+*****
+*****
+n =5
+for i in range(n):
+    for j in range(n):
+        print("*", end="")
+    print()
+'''
+
+##problem 2##
+##Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+'''
+*
+**
+***
+****
+*****
+
+n = 5
+for i in range(n):
+    for j in range(i+1):
+        print("*" , end ="")
+    print()
+'''
+
+##problem 3##
+##Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+'''
+1
+12
+123
+1234
+12345
+
+n = 5
+for i in range(n):
+    for j in range(i+1):
+        print(j+1 , end = "")
+    print()
+'''
