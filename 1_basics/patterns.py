d77d7e2b887dd49dcb5f2b1dc7756922b4eaa097
payaldehru-2007 <payaldehru@gmail.com>
2026-06-27 17:14:20 +0530

    Day 3: Pattern P9)

--- 1_basics/patterns.py
+++ 1_basics/patterns.py
@@ -137,3 +137,32 @@ for i in range(n):
         print("*",end ="")
     print()
 '''
+
+##problem 9##
+##Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+'''
+    * 
+   ***
+  *****
+ *******
+*********
+*********
+ *******
+  *****
+   ***
+    *
+
+n = 5
+for i in range(n):
+    for j in range(n-i-1):
+        print(" ",end="")
+    for j in range(2*i+1):
+        print("*",end="")
+    print()
+for i in range(n):
+    for j in range(i):
+        print(" ",end="")
+    for j in range(2*(n-i)-1):
+        print("*",end="")
+    print()
+'''
