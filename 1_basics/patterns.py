0ad84f83a1ea2324c0a08b6e23b62dd4b26b79cc
payaldehru-2007 <payaldehru@gmail.com>
2026-06-28 15:28:53 +0530

    Day 5: Pattern P12

--- 1_basics/patterns.py
+++ 1_basics/patterns.py
@@ -197,10 +197,30 @@ for i in range(n):
 1 0 1 
 0 1 0 1 
 1 0 1 0 1
-'''
 
 n = 5
 for i in range(n):
     for j in range(i+1):
         print(1 - ((i + j) % 2),end=" ")
     print()
+'''
+
+##problem 12##
+##Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+'''
+1        1
+12      21
+123    321
+1234  4321
+1234554321
+
+n = 5
+for i in range(n):
+    for j in range(i+1):
+        print(j+1,end="")
+    for j in range(2*(n-i-1)):
+        print(" ",end="")
+    for j in range(i+1):
+        print(i+1-j,end="")
+    print()
+'''
